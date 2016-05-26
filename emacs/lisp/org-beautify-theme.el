4182964e2084528ed8cf401e238366c0604dc779
Carter-Pro <18686883412@163.com>
2016-05-26 16:23:06 +0800

    add appointment notify function to org mode

---
+++ emacs/lisp/org-beautify-theme.el
@@ -0,0 +1,15 @@
+(defun org-src-color-blocks ()
+  "Colors the block headers and footers to make them stand out more for dark themes"
+  (interactive)
+  (custom-set-faces
+   '(org-block-begin-line
+     ((t (:foreground "#008ED1" :background "#002E41"))))
+   '(org-block-background
+     ((t (:background "#000000"))))
+   '(org-block
+     ((t (:background "#000000"))))
+   '(org-block-end-line
+     ((t (:foreground "#008ED1" :background "#002E41"))))
+
+   '(mode-line-buffer-id ((t (:foreground "black" :bold t))))
+   '(which-func ((t (:foreground "green"))))))
